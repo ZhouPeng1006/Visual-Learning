d18d5fc0a4eba44bf7abd43567a75c6db0052cf0
ZP <1725883850@qq.com>
2023-06-08 16:46:52 +0800

    init project

--- visual-learning-service/src/main/java/com/example/visuallearningservice/VisualLearningServiceApplication.java
+++ visual-learning-service/src/main/java/com/example/visuallearningservice/VisualLearningServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VisualLearningServiceApplication {
-
+//    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
     public static void main(String[] args) {
         SpringApplication.run(VisualLearningServiceApplication.class, args);
     }
