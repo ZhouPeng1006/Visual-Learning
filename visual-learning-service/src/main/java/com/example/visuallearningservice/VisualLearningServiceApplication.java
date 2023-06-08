b62c082dccc7e35c4a45fdcfc84170e0b057ed1c
ZP <1725883850@qq.com>
2023-06-08 16:36:40 +0800

    project init

---
+++ visual-learning-service/src/main/java/com/example/visuallearningservice/VisualLearningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.visuallearningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualLearningServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisualLearningServiceApplication.class, args);
+    }
+
+}
---
+++ visual-learning-service/src/test/java/com/example/visuallearningservice/VisualLearningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visuallearningservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualLearningServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
